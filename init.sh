9788ec82c573bfa94aa4624f49b50bb7bf514809
Jerry <jerryq27+dev@gmail.com>
2024-07-09 01:52:17 -0500

    Updated compose and init.sh to match old compose.

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+# TODO: Add checks if bundler and Jekyll have been installed
 # gem install bundler
 # bundle install
-jekyll serve --host 0.0.0.0
+jekyll serve --host 0.0.0.0 --livereload-port 4001 --livereload

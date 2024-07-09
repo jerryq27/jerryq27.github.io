414628280de0b059ab5956cc223eb4329c8c4ff8
Jerry <jerryq27+dev@gmail.com>
2024-07-09 02:24:39 -0500

    Checking if dependencies are installed before running installs.

--- init.sh
+++ init.sh
@@ -1,4 +1,16 @@
-# TODO: Add checks if bundler and Jekyll have been installed
-# gem install bundler
-# bundle install
+# Check if the correct dependencies are installed.
+if [[ $(gem list -i "^bundler$") == true ]]; then
+    echo "Found bundler"
+else
+    echo "Bundler not detected, installing..."
+    gem install bundler
+fi
+
+if [[ $(gem list -i "^jekyll$") == true ]]; then
+    echo "Found Jekyll"
+else
+    echo "Jekyll not detected, installing.."
+    bundle install
+fi
+
 jekyll serve --host 0.0.0.0 --livereload-port 4001 --livereload

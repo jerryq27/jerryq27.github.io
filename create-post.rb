d2e4601164f1601f8318f45b086a9a61155bcf97
Jerry <jerryq27+dev@gmail.com>
2024-07-10 04:20:31 -0500

    Creating Ruby script to create new posts.

---
+++ create-post.rb
@@ -0,0 +1,10 @@
+# YYYY-MM-DD-TITLE.EXTENSION
+
+# Ask for new post name
+# Get date and convert post to format
+# Create new file with the format name
+# Input default front matter and original post name as first heading
+
+print "Post name: "
+post_name = gets
+print post_name
